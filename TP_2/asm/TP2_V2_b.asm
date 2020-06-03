1085007f5e1caabe024a671a5adb51886cd0c8bf
mreigada <66037646+mreigada@users.noreply.github.com>
2020-06-03 19:00:00 -0300

    Add files via upload

---
+++ TP_2/asm/TP2_V2_b.asm
@@ -0,0 +1,53 @@
+;*********************************************************************************************************************
+;Encendido de un led ubicado en PORTB.2, mediante botón ubicado en PORTB.0 (usando resistencias pull-up internas)
+;
+; Alumno: Reigada Maximiliano Daniel
+; Padrón: 100565
+;*********************************************************************************************************************
+
+.INCLUDE "M328PDEF.INC"
+
+.EQU PUERTO_SALIDA = PORTB
+.EQU PUERTO_ENTRADA = PINB
+.EQU CONF_PUERTO = DDRB
+.EQU BOTON = 0
+.EQU LED = 2
+
+.CSEG
+.ORG 0x0000	
+	 JMP MAIN
+
+.ORG INT_VECTORS_SIZE 
+
+MAIN:
+								;Se inicializa el Stack Pointer al final de la RAM 
+	LDI R16, HIGH(RAMEND)		
+	OUT SPH, R16				
+	LDI R16, LOW(RAMEND)		
+	OUT SPL, R16	
+								; Se inicializan puertos 
+	LDI R17, 0X01
+	LDI R18, 0x24
+	OUT CONF_PUERTO, R18
+	OUT PUERTO_SALIDA, R17
+								; Se controla el LED
+CONTROLAR_LED:
+	CALL DETECTAR_ALTO			; Se espera hasta que el botón en el puerto de entrada sea presionado
+	CBI	 PUERTO_SALIDA, LED   
+	CALL DETECTAR_BAJO			; Se espera hasta que el botón en el puerto de entrada deje de estar presionado
+	SBI	 PUERTO_SALIDA, LED   
+	JMP  CONTROLAR_LED			; Se reinicia el ciclo de control
+	
+
+	
+								; Detecta flancos bajos en pin conectado al botón
+DETECTAR_BAJO: 
+	SBIC PUERTO_ENTRADA, BOTON
+	JMP  DETECTAR_BAJO
+	RET
+
+								; Detecta flancos altos en pin conectado al botón
+DETECTAR_ALTO:
+	SBIS PUERTO_ENTRADA, BOTON
+	JMP  DETECTAR_ALTO
+	RET
